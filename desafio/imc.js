fb62ed6bc3cc34d6fe5c96537aa41d39b12797ff
Wellington Cid <wellington.cid@hotmail.com>
2020-06-16 09:59:33 -0300

    challenge1

---
+++ desafio/imc.js
@@ -0,0 +1,15 @@
+const nome = 'Carlos'
+const peso = 90
+const altura = 1.88
+
+const imc = peso / (altura * altura)
+
+let message = ""
+
+if (imc > 30){
+    message = `${nome}, seu imc é ${imc}, você está acima do peso`
+} else {
+    message = `${nome}, seu imc é ${imc}, seu peso está controlado`
+}
+
+console.log(message)
